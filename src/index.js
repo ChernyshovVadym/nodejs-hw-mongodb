bf5cfd9fddd5c6de7dc7a338142397d82cebcf46
ChernyshovVadym <vad14@i.ua>
2024-06-04 22:11:22 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { setupServer } from './server';
 
+import { initMongoConnection } from './db/initMongoConnection.js';
+
 const getContacts = async () => {
   await initMongoConnection();
   setupServer();
