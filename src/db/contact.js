de085807eb89e13858b3f4cca5491921c4eb5680
ChernyshovVadym <vad14@i.ua>
2024-06-05 16:13:47 +0300

    add

--- src/db/contact.js
+++ src/db/contact.js
@@ -1,6 +1,34 @@
-const contactsShema = new Schema()({
-  name: {
-    type: String,
-    required: true,
+import { Schema, model } from 'mongoose';
+
+const contactsShema = new Schema()(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      optional: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
   },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Contact = model('contacts', contactsShema);
--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,6 +4,11 @@ import { env } from '../utils/env';
 export const initMongoConnection = async () => {
   try {
     const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+    await mongoose.connect(``);
+    console.log('Mongo connection successfully established!');
   } catch (error) {
     console.log(error);
     throw error;
