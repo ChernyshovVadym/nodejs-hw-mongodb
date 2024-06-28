924311d93eaeab8abb6baf7600177bc51538d3e1
ChernyshovVadym <vad14@i.ua>
2024-06-28 11:53:24 +0300

    add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -57,7 +57,8 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
-  const contact = await createContact(req.body);
+  const userId = req.user.id;
+  const contact = await createContact(req.body, userId);
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
