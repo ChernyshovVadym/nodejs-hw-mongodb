6461bed600e241e76aa803ab911d383bc171447e
ChernyshovVadym <vad14@i.ua>
2024-07-04 14:13:08 +0300

    add

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -11,6 +11,9 @@ import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
+import { env } from '../utils/env.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { saveFileToUploadDir } from '../utils/createDirIfNotExists.js';
 
 import mongoose from 'mongoose';
 
@@ -58,7 +61,17 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res) => {
   const userId = req.user.id;
-  const contact = await createContact({ ...req.body, userId });
+  const photo = req.file;
+  let photoUrl;
+  if (photo) {
+    if (env('ENABLE_CLODINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({ ...req.body, photo: photoUrl }, userId);
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
